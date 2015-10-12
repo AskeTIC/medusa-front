9bc792d71ab414172f52a61d0dd8f076708a0020
jonabasque <jonmultimedia@gmail.com>
2015-10-12 14:54:48 +0200

    Colocado el measuresController en la directiva -aside-left- por que allí van en principio los measures, lo del main serán -charts- o algo parecido

--- js/app.js
+++ js/app.js
@@ -2,14 +2,6 @@
 	//Creamos la app de Angular que es un módulo -medusa- sin dependencias
 	var app = angular.module('measures', []);
 	
-	app.controller('measuresController', function(){
-		this.measure = {
-			name : 'temperatura',
-			data : '34',
-			date : '12:00:02:04' 
-		};
-	});
-
 	//DIRECTIVAS PARA EXTRUCTURA
 	app.directive('asideLeft', function(){
 		return{
@@ -17,6 +9,15 @@
 			restrict: 'E',
 			//Le indicamos la URL
 			templateUrl: '/partials/aside-left.html'
+			controller: function(){
+				this.measure = {
+	                        	name : 'temperatura',
+        	                	data : '34',
+                	        	date : '12:00:02:04'
+                		}:
+
+			}
+			controllerAs: 'meCtl'
 		}
 	});
 
