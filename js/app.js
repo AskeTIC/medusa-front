788f48118ff6e4f90ac6d07b3b11b4a5ff2705da
jonabasque <jonmultimedia@gmail.com>
2015-10-26 01:29:16 +0100

    Refactorizados los controllers y modularizada la app

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 (function(){
+	"use strict";
 	//Creamos la app de Angular que es un módulo -medusa- sin dependencias
 	var app = angular.module('medusa', [
 		'ngRoute',
 		'medusa.directives',
 		'medusa.controllers'
 	])
+	.config(config);
 
-	.config(['$routeProvider', function($routeProvider){
+	function config ($locationProvider, $routeProvider){
+		$locationProvider.html5Mode(true);
 		$routeProvider
 			.when('/', {
 				templateUrl: 'views/principal.html'
@@ -20,7 +23,6 @@
 			.otherwise({
 				redirectTo: '/'
 			});
-
-	}]);
+	}
 
 })();
--- js/controllers.js
+++ js/controllers.js
@@ -1,79 +1,36 @@
 (function(){
-
-	angular.module('medusa.controllers', [])
-
-		.controller('PrincipalController', ['$scope', '$http', function($scope, $http){
-
-		}])
-
-		.controller('PruebasController', ['$scope', '$http', function($scope, $http){
-
-		}])
-
-		.controller('CamarasController', ['$scope', '$http', function($scope, $http){
-
-		}])
+	"use strict";
+	angular
+		.module('medusa.controllers', ['medusa.services'])
+		.controller('PrincipalController', PrincipalController)
+		.controller('PruebasController', PruebasController)
+		.controller('CamarasController', CamarasController)
 		//Recoge las measures y las ordena en el orden elegido
-		.controller('AsideLeftController',['$scope', function($scope){
-			this.atmosphericMeasures = [];
-			this.maritimeMeasures = [];
-		}])
+		.controller('AsideLeftController', AsideLeftController)
 		//Envía las orders y las ordena en el orden elegido
-		.controller('AsideRightController', ['$scope', function($scope){
-			this.trialOrders= [];
-			this.generalOrders = [];
-		}])
+		.controller('AsideRightController', AsideRightController)
+		.controller('MeasuresController', MeasuresController);
 
-		.controller('MeasuresController', ['$scope', '$http', function($scope, $http){
+	function PrincipalController(){
 
-			//Atmospherics measures
-			this.atmospherics = [{
-				'name': 'viento',
-				'imageUrl':'images/imagen1.png',
-				'data':'20.5',
-				'measure':'km/h',
-				'class':'left-red'
-				},
-				{
-				'name': 'temperatura',
-				'imageUrl':'images/imagen2.png',
-				'data':'30.1',
-				'measure':'º C',
-				'class':'left-green'
-				},
-				{
-				'name': 'presion',
-				'imageUrl':'images/imagen3.png',
-				'data':'40.6',
-				'measure':'km/h',
-				'class':'left-red'
-				},
-				{
-				'name': 'humedad',
-				'imageUrl':'images/imagen4.png',
-				'data':'60',
-				'measure':'kg cm2',
-				'class':'left-green'
-				}
-			];
+	}
 
-			//Maritime measures
-			this.maritime= [{
-				'name': 'altura',
-				'imageUrl':'images/imagen5.png',
-				'data':'1.75',
-				'measure':'m',
-				'class':'left-red'
-				},
-				{
-				'name': 'distancia',
-				'imageUrl':'images/imagen6.png',
-				'data':'3.54',
-				'measure':'seg',
-				'class':'left-green'
-				}
-			];
+	function PruebasController(){
 
-		}]);
+	}
+
+	function CamarasController(){
+
+	}
+
+	function AsideLeftController(Measures){
+		this.atmosphericMeasures = [];
+		this.maritimeMeasures = [];
+	}
+
+	function AsideRightController(Measures){
+		this.trialOrders= [];
+		this.generalOrders = [];
+	}
 
 })();
