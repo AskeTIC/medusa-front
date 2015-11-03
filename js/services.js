5cb03e643b0b57bbf2d24228417d5d662ef12260
jonabasque <jonmultimedia@gmail.com>
2015-11-03 16:56:20 +0100

    Cambiado el módulo cargado en el index.html

--- js/services.js
+++ js/services.js
@@ -2,7 +2,7 @@
     "use strict";
 
     angular
-        .module('medusa.services', ['ngWebSocket'])
+        .module('medusa.services', ['btford.socket-io'])
         .factory('AtmosphericsSensors', atmosphericsSensors)
         .factory('StrongsSensors', strongsSensors)
         .factory('InclinometersSensors', inclinometersSensors)
@@ -14,41 +14,41 @@
         .factory('LapCountersSensors', lapCountersSensors)
         .factory('GuidesSensors', guidesSensors);
 
-        function atmosphericsSensors($websocket){
-            var ws = $websocket('ws://localhost:3030');
-            var sensors = [];
+        function atmosphericsSensors(socketFactory){
+          var ws = $websocket('ws://localhost:3030');
+          var sensors = [];
 
-            ws.onMessage(function(event) {
-                console.log('AtmosphericsSensors: ', event);
-                var res;
-                res = JSON.parse(event.data);
-                /*
-                try {
-                    res = JSON.parse(event.data);
-                } catch(e) {
-                    res = {'username': 'anonymous', 'message': event.data};
-                }
-                */
-                sensors.push({
-                    data : res.data,
-                    date : res.date
-                });
-            });
+          ws.onMessage(function(event) {
+              console.log('AtmosphericsSensors: ', event);
+              var res;
+              res = JSON.parse(event.data);
+              /*
+              try {
+                  res = JSON.parse(event.data);
+              } catch(e) {
+                  res = {'username': 'anonymous', 'message': event.data};
+              }
+              */
+              sensors.push({
+                  data : res.data,
+                  date : res.date
+              });
+          });
 
-            ws.onError(function(event) {
-              console.log('connection Error', event);
-            });
+          ws.onError(function(event) {
+            console.log('connection Error', event);
+          });
 
-            ws.onClose(function(event) {
-              console.log('connection closed', event);
-            });
+          ws.onClose(function(event) {
+            console.log('connection closed', event);
+          });
 
-            ws.onOpen(function() {
-              console.log('connection open');
-              ws.send('Hello World');
-              ws.send('again');
-              ws.send('and again');
-            });
+          ws.onOpen(function() {
+            console.log('connection open');
+            ws.send('Hello World');
+            ws.send('again');
+            ws.send('and again');
+          });
 
             return {
                 measures: sensors
