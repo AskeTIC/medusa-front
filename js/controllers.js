5e31e65b768ec383ce5195c1fa88a4beb0c0791e
jonabasque <jonmultimedia@gmail.com>
2015-11-06 15:27:44 +0100

    Insertado OrderController en las ordenes, mostradas con ng-repeat y cambio de this a

--- js/controllers.js
+++ js/controllers.js
@@ -9,8 +9,14 @@
 		.controller('AsideLeftController', AsideLeftController)
 		//Envía las orders y las ordena en el orden elegido
 		.controller('AsideRightController', AsideRightController)
+		//Controller de Orders
+		.controller('OrdersController', cbOrdersController)
+
+		//Recoge los datos de los sensores atmosfericos, los une en JSON y hace las formulas correspondientes.
 		.controller('AtmosphericsSensorsController', cbAtmosphericsSensorsController);
 
+
+	//CONTROLADORES QUE DEFINEN EL ORDEN DE LOS ELEMENETOS, ESTABLECIDOS POR ROL X.
 	function PrincipalController(){
 
 	}
@@ -23,33 +29,32 @@
 
 	}
 
-	function AsideLeftController(AtmosphericsSensors){
-		this.atmosphericMeasures = [];
-		this.maritimeMeasures = [];
-		//$scope.sensors = AtmosphericsSensors.data;
+	function AsideLeftController($scope){
+		$scope.atmosphericMeasures = [];
+		$scope.maritimeMeasures = [];
 
 		//Atmospherics measures
-		//TODO: recorrer el array measures de la factoría AtmosphericsSensors
-		//y con el atributo -name- crear el indice, con imageURL, data y measure el resto.
-		this.atmosphericMeasures.push({
+		//TODO: recorrer el array measures de la factoría AtmosphericsSensors y asignar la data.
+		//TODO: recoger los sensors ordenados de la configuración y crear el array atmosphericMeasures dinamicamente.
+		$scope.atmosphericMeasures.push({
 				'name': 'viento',
 				'imageUrl':'images/imagen1.png',
 				'data': '¿?',
 				'measure':'km/h'
-		}) ;
-		this.atmosphericMeasures.push({
+		});
+		$scope.atmosphericMeasures.push({
 				'name': 'temperatura',
 				'imageUrl':'images/imagen2.png',
 				'data': '¿?',
 				'measure':'º C'
 		});
-		this.atmosphericMeasures.push({
+		$scope.atmosphericMeasures.push({
 				'name': 'presion',
 				'imageUrl':'images/imagen3.png',
 				'data': '¿?',
 				'measure':'kg cm2'
 		});
-		this.atmosphericMeasures.push({
+		$scope.atmosphericMeasures.push({
 				'name': 'humedad',
 				'imageUrl':'images/imagen4.png',
 				'data': '¿?',
@@ -57,26 +62,70 @@
 		});
 
 		//Maritime measures
-		this.maritimeMeasures.push({
+		$scope.maritimeMeasures.push({
 				'name': 'altura',
 				'imageUrl':'images/imagen5.png',
 				'data': '¿?',
 				'measure':'m'
 		});
-		this.maritimeMeasures.push({
+		$scope.maritimeMeasures.push({
 				'name': 'distancia',
 				'imageUrl':'images/imagen6.png',
 				'data': '¿?',
 				'measure':'seg'
 		});
 
-		console.log(this.atmosphericMeasures);
+	}
+
+	function AsideRightController($scope){
+		$scope.orders= [];
+
+		//Ordenes de ensayos
+		//TODO: recoger los sensors ordenados de la configuración y crear el array trialOrders dinamicamente.
+		$scope.orders.push({
+			'name': 'porcentajeCarga',
+			'type' : 100
+	 	});
+
+		$scope.orders.push({
+			'name': 'carenaCarga',
+			'type': 4
+	 	});
+
+		$scope.orders.push({
+			'name': 'manejoVela',
+			'type': 0
+	 	});
+
+		$scope.orders.push({
+			'name': 'inclinacionVela',
+			'type': 10
+	 	});
+
+		$scope.orders.push({
+			'name': 'porcentajeAnillo',
+			'type': 20
+	 	});
+
+		//Ordenes generales
+		//TODO: recoger los sensors ordenados de la configuración y crear el array generalOrders dinamicamente.
+		$scope.orders.push({
+			'name': '',
+			'type' : 101
+	 	});
 
 	}
 
-	function AsideRightController(){
-		this.trialOrders= [];
-		this.generalOrders = [];
+	//Callbacks de controladores de orders
+	function cbOrdersController($scope){
+			//TODO: Recorrer los 2 arrays o juntarlos y evaluar si su tipo es mayor de 100... (controlo que sea orden general)
+			//recorro el array que me indiquen por parámetro.
+			for(var order in $scope.orders){
+					if(order.type === 20){
+							$scope.orders[order].action = actionOrders.type(20);
+					}
+					console.log($scope.order);
+			}
 	}
 
 	function cbAtmosphericsSensorsController(AtmosphericsSensors){
