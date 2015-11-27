6c133339c139c2664d99834cfd4ac6ee5514ecb1
jonabasque <jonmultimedia@gmail.com>
2015-11-27 17:36:56 +0100

    Agregado funcion parse() para elegir opción a parsear Int o Float

--- js/calculus.js
+++ js/calculus.js
@@ -7,54 +7,78 @@
         .factory('Calc', cbCalc);
 
         function cbCalc(){
+			//Parsear un string a int o float
+		var parse = function(string, parse){
+			var result;
+			if(parse === "float"){
+				result = parseFloat(string);
+			}else{
+				result = parseInt(string);
+			}
+			return result;
+		};
+
 		//Suma de uno o más números
-		var sumar = function(nums, num){
+		var sumar = function(nums, num, number){
 			var num2 = num || null;
+			var number = number || "float";
 			var result = 0;
 			if(num2 === null){
 				for (var n in nums) {
-					result = result + nums[n];
+					result = parse(result, number) + nums[n];
 				}
 			}else{
-				result = (parseInt(nums) + parseInt(num));
-
+				result = (parse(nums, number) + parse(num, number));
 			}
 
 			return result;
 		};
 
 		//restar 2 números
-		var restar = function(num1, num2){
-			return (parseInt(num1) + parseInt(num2));
+		var restar = function(num1, num2, number){
+			var number = number || "float";
+			return (parse(num1, number) + parse(num2, number));
 		};
 
 		//multiplicar 2 números
-		var multiplicar = function(num1, num2){
-			return (parseInt(num1) * parseInt(num2));
+		var multiplicar = function(num1, num2, number){
+			var number = number || "float";
+			return (parse(num1, number) * parse(num2, number));
 		};
 
 		//Dividir 2 números
-		var dividir = function(divisor, dividendo){
-			return (parseInt(divisor) / parseInt(dividendo));
-		};
+	    	var dividir = function(divisor, dividendo, number){
+			var number = number || "float";
+			return (parse(divisor, number) / parse(dividendo, number));
+	    	};
 
 		//Obtener la tangente de un grado
-		var get_tangent = function(degree){
-			return Math.tan(degree);
+		var get_tangent = function(degree, number){
+			var number = number || "float";
+			return Math.tan(parse(degree, number));
 		};
 
-		var get_atangent = function(degree){
-			return Math.atan(degree);
+		var get_atangent = function(degree, number){
+			var number = number || "float";
+			return Math.atan(parse(degree, number));
+		};
+
+		//Calcular grados desde radianes
+		var get_degree = function(radian, number){
+			var number = number || "float";
+			return parse(radian, number) * 180 / Math.PI;
 		};
 
 		//Calcular el radian de un grado
 		var get_radian = function(degree){
-		    	return parseInt(degree) * Math.PI / 180;
+			var number = number || "int";
+		    return parse(degree, number) * Math.PI / 180;
 		};
 
 		//calcular una potencia
 		var get_power = function(degree){
-			return parseInt(Math.pow(degree));
+			var number = number || "int";
+			return Math.pow(parse(degree, number));
 		};
 
 		return {
@@ -65,7 +89,8 @@
 			div: dividir,
 			tang: get_tangent,
 			atang: get_atangent,
-			radian: get_radian
+			radian: get_radian,
+			degree: get_degree
 		}
         }
 })();
